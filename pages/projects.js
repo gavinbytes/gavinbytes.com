b6b4f9d4a3e506af9c13118809bc9ca7a6184b2c
Gavin Aguinaga <gavin.aguinaga@yahoo.com>
2023-04-10 11:58:32 -0700

    updated project page to have actual content

--- pages/projects.js
+++ pages/projects.js
@@ -1,48 +1,30 @@
 import React from 'react';
 import Layout from '../components/Layout';
 import { Card, Grid, Row, Text } from "@nextui-org/react";
+import Link from 'next/link';
 
 function Projects() {
   const list = [
     {
-      title: "Orange",
-      img: "/images/fruit-1.jpeg",
-      price: "$5.50",
+      title: "Medicine Tracker",
+      img: "/medicineTracker.png",
+      github: "code",
+      link: 'https://youtu.be/cw38Kft5ctE',
+      gitlink: 'https://github.com/gavinbytes/Medicine-Tracker',
     },
     {
-      title: "Tangerine",
-      img: "/images/fruit-2.jpeg",
-      price: "$3.00",
+      title: "MyPantry",
+      img: "/myPantryPic.png",
+      github: "code",
+      link: '/MyPantryPoster.pdf',
+      gitlink: 'https://github.com/My-PantryGit/My_Pantry_Main',
     },
     {
-      title: "Cherry",
-      img: "/images/fruit-3.jpeg",
-      price: "$10.00",
-    },
-    {
-      title: "Lemon",
-      img: "/images/fruit-4.jpeg",
-      price: "$5.30",
-    },
-    {
-      title: "Avocado",
-      img: "/images/fruit-5.jpeg",
-      price: "$15.70",
-    },
-    {
-      title: "Lemon 2",
-      img: "/images/fruit-6.jpeg",
-      price: "$8.00",
-    },
-    {
-      title: "Banana",
-      img: "/images/fruit-7.jpeg",
-      price: "$7.50",
-    },
-    {
-      title: "Watermelon",
-      img: "/images/fruit-8.jpeg",
-      price: "$12.20",
+      title: "Clock of Clocks",
+      img: "./clocks.png",
+      github: "code",
+      link: '/clockwall',
+      gitlink: 'https://github.com/gavinbytes/gavinbytes.com/blob/main/pages/clockwall.js',
     },
   ];
 
@@ -52,23 +34,27 @@ function Projects() {
         {list.map((item, index) => (
           <Grid xs={6} sm={3} key={index}>
             <Card isPressable>
-              <Card.Body css={{ p: 0 }}>
-                <Card.Image
-                  src={"https://nextui.org" + item.img}
-                  objectFit="cover"
-                  width="100%"
-                  height={140}
-                  alt={item.title}
-                />
-              </Card.Body>
-              <Card.Footer css={{ justifyItems: "flex-start" }}>
-                <Row wrap="wrap" justify="space-between" align="center">
-                  <Text b>{item.title}</Text>
-                  <Text css={{ color: "$accents7", fontWeight: "$semibold", fontSize: "$sm" }}>
-                    {item.price}
-                  </Text>
-                </Row>
-              </Card.Footer>
+                <Card.Body css={{ p: 0 }}>
+                  <Link href={item.link} passHref>
+                    <Card.Image
+                      src={item.img}
+                      objectFit="cover"
+                      width="100%"
+                      height={140}
+                      alt={item.title}
+                    />
+                  </Link>
+                </Card.Body>
+                <Card.Footer css={{ justifyItems: "flex-start" }}>
+                  <Row wrap="wrap" justify="space-between" align="center">
+                    <Text b>{item.title}</Text>
+                    <Link href={item.gitlink} passHref>
+                      <Text isHoverable css={{ color: "$accents7", fontWeight: "$semibold", fontSize: "$sm" }}>
+                        {item.github}
+                      </Text>
+                    </Link>
+                  </Row>
+                </Card.Footer>
             </Card>
           </Grid>
         ))}
